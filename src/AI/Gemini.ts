c963b1495eb4ccbf77a6b75a3a6681abcd7030b7
JoaoPauloQC <joaopauloqueirozcosta@gmail.com>
2025-07-23 00:22:28 -0300

    fix: return of ai new working but it doesn´t appear yet

--- src/AI/Gemini.ts
+++ src/AI/Gemini.ts
@@ -1,23 +1,14 @@
-export const Gemini = async () =>{
-    const apiKey: string = "AIzaSyARvRcP6bNEWli5YRZLNKPXb-OagJsqEbU"
-    return (await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=${apiKey}`,
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                contents : [
-                    {
-                        role: "user",
-                        parts: [
-                            {
-                            text: "Você precisa buscar uma noticia recente em algum site e retornar um texto contendo o titulo, categoria, link dela e link da imagem dela. Então o retorno esperado deverá ser algo do tipo: Presidente faz isso; geral; (link) ; (link da img). Não retorne qualquer outra informação"
-                            }
-                        ]
-                    }
-                ]
-        })
-        }
-        ).then(res => res.json()))
-}
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey:  "AIzaSyARvRcP6bNEWli5YRZLNKPXb-OagJsqEbU" })
+
+export default async function Gemini (){
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: "Você precisa buscar uma noticia recente em algum site e retornar um texto contendo o titulo, categoria, link dela e link da imagem dela. Então o retorno esperado deverá ser algo do tipo: Presidente faz isso; geral; link ; link da img. Não retorne qualquer outra informação"
+    })
+    console.log(response.text)
+    return (await response.text)
+
+    }
+    
--- src/areas/GeminiArea.tsx
+++ src/areas/GeminiArea.tsx
@@ -1,4 +1,4 @@
-import { Gemini } from "@/AI/Gemini";
+import Gemini  from "@/AI/Gemini";
 import { Button } from "@/components/Buttons";
 
 export default function GeminiArea() {
@@ -6,7 +6,7 @@ export default function GeminiArea() {
     return (
         <div className="CHATGPT flex pb-10">
             <div className="wrapper">
-                <Button className="CHATGPT__button py-2 px-5 rounded-xl " onClick={() => console.log(Gemini())} animation="geminibtn"> Seja atualizado </Button>
+                <Button className="CHATGPT__button py-2 px-5 rounded-xl " onClick={async () => {await Gemini()}} animation="geminibtn"> Seja atualizado </Button>
             </div>
         </div>
     )
